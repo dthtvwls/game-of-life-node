9c8ccbca4f2af6ffe74093bbb1983180a2e28194
Joshua Stauter <joshua@joshuastauter.com>
2012-08-19 01:23:11 -0400

    worst sound ever

--- public/js/script.js
+++ public/js/script.js
@@ -1,14 +1,27 @@
 (function () {
   
-  var socket = io.connect(location.origin),
-      canvas = document.getElementById("canvas"),
-      ctx    = canvas.getContext("2d"),
-      rainbow  = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"],
-      cellSize = 10,
-      drawCell = function (x, y) {        
-        ctx.fillStyle = rainbow[Math.round(Math.random() * 6)];
-        ctx.fillRect(x * cellSize, y * cellSize, cellSize, cellSize);
+  var socket     = io.connect(location.origin),
+      canvas     = document.getElementById("canvas"),
+      canvasCtx  = canvas.getContext("2d"),
+      audioCtx   = new webkitAudioContext();
+      audioNode  = audioCtx.createJavaScriptNode(1024, 1, 1);
+      rainbow    = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"],
+      cellSize   = 10,
+      sine       = 0,
+      scale      = [523.25, 587.33, 659.26, 698.46, 783.99, 880.00, 987.77],
+      frequency  = audioCtx.sampleRate / (440 * Math.PI),
+      drawCell   = function (x, y) {        
+        canvasCtx.fillStyle = rainbow[Math.round(Math.random() * 6)];
+        canvasCtx.fillRect(x * cellSize, y * cellSize, cellSize, cellSize);
       };
+  
+  audioNode.onaudioprocess = function (e) {
+    var i, data = e.outputBuffer.getChannelData(0);
+    for (i = 0; i < data.length; ++i) {
+      data[i] = Math.sin(sine++ / frequency);
+    }
+  };
+  audioNode.connect(audioCtx.destination);
       
   canvas.addEventListener("mousemove", function (e) {
     var x = Math.floor((e.pageX - canvas.offsetLeft) / cellSize),
@@ -19,13 +32,14 @@
   }, false);
 
   socket.on("world", function (data) {
+    
+    frequency = audioCtx.sampleRate / (scale[Math.round(Math.random() * 6)] * Math.PI);
 
     var x, y, world = data.world;
 
     // white out the whole canvas
-    ctx.fillStyle = "white";
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-    ctx.fillStyle = "black";
+    canvasCtx.fillStyle = "white";
+    canvasCtx.fillRect(0, 0, canvas.width, canvas.height);
 
     // World matrix is column-major
     for (y = 0; y < world.length; y++) {
